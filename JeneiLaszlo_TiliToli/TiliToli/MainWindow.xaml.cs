76098f671fc00a24c4c2c404b652b83cc34855da
JeneiLaszlo <laceehyt@gmail.com>
2020-12-17 18:49:43 +0100

    Add files via upload

--- JeneiLaszlo_TiliToli/TiliToli/MainWindow.xaml.cs
+++ JeneiLaszlo_TiliToli/TiliToli/MainWindow.xaml.cs
@@ -26,6 +26,7 @@ namespace TiliToli
         }
         int[] allas = { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
         int[] kesz = { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
+        int lepesek;
         private void Button1_Click(object sender, RoutedEventArgs e)
         {
             Button ezGomb = sender as Button;
@@ -43,7 +44,8 @@ namespace TiliToli
                 var seged = ezGomb.Margin;
                 ezGomb.Margin = nullaGomb.Margin;
                 nullaGomb.Margin = seged;
-
+                lepesek++;
+                label.Content = "Lépések száma: " + lepesek;
                 allas[nullaGombIndex] = allas[ezGombFelirat];
                 allas[ezGombFelirat] = 0;
 
@@ -58,6 +60,31 @@ namespace TiliToli
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Button0.Visibility = Visibility.Hidden;
+            lepesek = 0;
+            Random r = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                Button ezGomb = (Button)FindName("Button" + Convert.ToString(r.Next(1, 9)));
+                Button nullaGomb = (Button)FindName("Button0");
+
+                var fTav = Math.Abs(ezGomb.Margin.Top - nullaGomb.Margin.Top);
+                var vTav = Math.Abs(ezGomb.Margin.Left - nullaGomb.Margin.Left);
+
+                int ezGombFelirat = int.Parse(ezGomb.Content.ToString());
+                int ezGombIndex = Array.IndexOf(allas, ezGombFelirat);
+                int nullaGombIndex = Array.IndexOf(allas, 0);
+
+                if ((fTav == 100 && vTav == 0) || (vTav == 100 && fTav == 0))
+                {
+                    var seged = ezGomb.Margin;
+                    ezGomb.Margin = nullaGomb.Margin;
+                    nullaGomb.Margin = seged;
+
+                    var seged2 = ezGombFelirat;
+                    allas[nullaGombIndex] = seged2;
+                    allas[ezGombIndex] = 0;
+                }
+            }
         }
     }
 }
